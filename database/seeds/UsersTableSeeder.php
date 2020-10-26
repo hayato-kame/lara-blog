95349eb737de7a4c20ca6b02e218260d9cfbbacc
Ubuntu <ubuntu@ip-172-31-24-110.ap-northeast-1.compute.internal>
2020-10-26 06:18:50 +0000

    add seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        $fnames = ["佐藤", "鈴木", "高橋", "田中"];
+        $gnames = ["太郎", "次郎", "花子"];
+        
+       // 注意 シングルクオーテーションで囲んだ文字列の中で変数を記述しても変数展開は行われません
+        
+        for($i = 1; $i <= 10; $i++) {
+            DB::table('users')->insert([
+                'name' => "${fnames[$i % 4]}" . "${gnames[$i % 3]}" ,
+                'email' => 'aaa' . $i . '@example.com',
+                'password' => Hash::make('password'. $i),
+            ]);
+        }
+        
+        
+        
+        for($i = 1; $i <= 10; $i++) {
+            DB::table('users')->insert([
+                'name' =>'テストユーザー' . $i,
+                'email' => 'bbb' .$i . '@example.com',
+                'password' => Hash::make('password'),
+            ]);
+        }
+        
+         for($i = 1; $i <= 30; $i++) {
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10) . '@example.com',
+                'password' => Hash::make('password'),
+            ]);
+        }
+    }
+}
